80ff81e6f09726e45e5aa159729aed8ae50d298e
Fasil Arakkal <fasil.arakkal@healthdirect.org.au>
2017-07-30 10:29:40 +1000

    Updated the Json to match

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -10,7 +10,7 @@ const ProjectItem = props => {
   return (
   <Media>
     <Media left href="#">
-      <Media object data-src={props.project.image}/>
+      <Media object src={props.project.image}/>
     </Media>
     <Media body>
       <Media heading>
@@ -24,19 +24,25 @@ const ProjectList = props => {
 
   let projects = [
     {
-      name: "Project name",
-      image: "project-image.jpg",
-      description:"Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras puru odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
+      name: "The central park",
+      image: "https://s3-ap-southeast-2.amazonaws.com/webifycentral/images/proj1.jpg",
+      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
       name: "Project name 1",
-      image: "project-image-1.jpg",
+      image: "https://s3-ap-southeast-2.amazonaws.com/webifycentral/images/proj2.png",
       description:"Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras puru odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
 
     },
     {
       name: "Project name 2",
-      image: "project-image-2.jpg",
+      image: "https://s3-ap-southeast-2.amazonaws.com/webifycentral/images/proj3.jpg",
+      description:"Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras puru odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
+
+    },
+    {
+      name: "Project name 2",
+      image: "https://s3-ap-southeast-2.amazonaws.com/webifycentral/images/proj4.png",
       description:"Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras puru odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Doneclacinia congue felis in faucibus."
 
     }
