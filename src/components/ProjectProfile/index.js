8faed8a299bd85caec3d99bbc30c45416a58075d
Ainharan Subramaniam <ainharan.subramaniam@gmail.com>
2017-07-30 12:08:56 +1000

    kinda fixed the stupid error

--- src/components/ProjectProfile/index.js
+++ src/components/ProjectProfile/index.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import { Container, Row, Col, Button,  Nav, NavItem, NavLink } from 'reactstrap';
 import Lightbox from 'react-images';
+const IMAGES = [
+    { src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg" },
+    { src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg" },
+    { src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg" }
+];
 
+export default class example extends React.Component {
+    constructor(props){
+        super(props);
+        this.onClose = this.onClose.bind(this);
+        this.gotoNext = this.gotoNext.bind(this);
+        this.gotoPrevious = this.gotoPrevious.bind(this);
+    }
 
-import s from './styles.scss';
-import moment from 'moment';
+    onClose(){
 
+    }
+    gotoNext(){
 
-export default class Sample extends React.Component {
+    }
+    gotoPrevious(){
 
-  render() {
-    return (
-      <Lightbox
-        images={[{ src: 'https://lonelyplanetimages.imgix.net/mastheads/65830387.jpg?sharp=10&vib=20&w=1200' }]}
-        isOpen={this.state.lightboxIsOpen}
-        onClickPrev={this.gotoPrevious}
-        onClickNext={this.gotoNext}
-        onClose={this.closeLightbox}
-      />
-    );
-  }
+    }
+    render(){
+        return (
+            <Lightbox
+                isOpen
+                images={IMAGES}
+                onClose={this.onClose}
+                onClickPrev={this.gotoPrevious}
+                onClickNext={this.gotoNext}
+            />
+        );
+    }
 }
-
